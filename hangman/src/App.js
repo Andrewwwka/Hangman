a658dc3fbcf768aedec1887038f924d6239a31d6
Andrew <andrewalthoff@outlook.com>
2025-12-01 22:49:50 -0600

    post structure added to Hangman game UI.

--- hangman/src/App.js
+++ hangman/src/App.js
@@ -38,33 +38,39 @@ function App() {
         <div>
           <p>Random Word: {word}</p>
           <button onClick={fetchRandomWord}>Fetch New Word</button>
+          <div className ="hangman-post">
+            <div className ="post-base"></div>
+            <div className ="post-vertical"></div>
+            <div className ="post-horizontal"></div>
+            <div className ="post-rope"></div>
+          </div>
           <div className="keyboard">
-          <button onClick>A</button>
-          <button onClick>B</button>
-          <button onClick>C</button>
-          <button onClick>D</button>
-          <button onClick>E</button>
-          <button onClick>F</button>
-          <button onClick>G</button>
-          <button onClick>H</button>
-          <button onClick>I</button>
-          <button onClick>J</button>
-          <button onClick>K</button>
-          <button onClick>L</button>
-          <button onClick>M</button>
-          <button onClick>N</button>
-          <button onClick>O</button>
-          <button onClick>P</button>
-          <button onClick>Q</button>
-          <button onClick>R</button>
-          <button onClick>S</button>
-          <button onClick>T</button>
-          <button onClick>U</button>
-          <button onClick>V</button>
-          <button onClick>W</button>
-          <button onClick>X</button>
-          <button onClick>Y</button>
-          <button onClick>Z</button>
+            <button onClick>A</button>
+            <button onClick>B</button>
+            <button onClick>C</button>
+            <button onClick>D</button>
+            <button onClick>E</button>
+            <button onClick>F</button>
+            <button onClick>G</button>
+            <button onClick>H</button>
+            <button onClick>I</button>
+            <button onClick>J</button>
+            <button onClick>K</button>
+            <button onClick>L</button>
+            <button onClick>M</button>
+            <button onClick>N</button>
+            <button onClick>O</button>
+            <button onClick>P</button>
+            <button onClick>Q</button>
+            <button onClick>R</button>
+            <button onClick>S</button>
+            <button onClick>T</button>
+            <button onClick>U</button>
+            <button onClick>V</button>
+            <button onClick>W</button>
+            <button onClick>X</button>
+            <button onClick>Y</button>
+            <button onClick>Z</button>
           </div>
         </div>
       )}
